9c830de3d14d3b5607ee0c7efc1c9ba97d074e62
astanciu <alexandru.stanciu@mozido.com>
2016-12-14 14:41:51 -0500

    New empty test project for testing custom I2C devices;

---
+++ etc/i2c_slave_test/i2c_slave_test.ino
@@ -0,0 +1,14 @@
+// Slave I2C Serial Interface Adapter Module test project.
+// address: 0x27
+#include <Wire.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
