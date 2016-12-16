cc5e7ad46289d3a0478075d85310741fe9ae8f63
astanciu <alexandru.stanciu@mozido.com>
2016-12-16 16:53:51 -0500

    Code review;

--- libs/ToggleButtonLib/examples/ToggleButtonTestCallback/ToggleButtonTestCallback.ino
+++ libs/ToggleButtonLib/examples/ToggleButtonTestCallback/ToggleButtonTestCallback.ino
@@ -13,9 +13,8 @@ bool _ledStatus = false;
 //=======   /* END OF: TIMER DEFS & VARIABLES */
 
 /* BUTTONS DEFS & VARIABLES */
-//#define BTN_PIN1 16 // pushbutton 1 (GPIO16 D0) - UV & daytime lamps;
-#define BTN_PIN1 5  // pushbutton 1 (GPIO5 D1) - UV & daytime lamps;
-#define BTN_PIN2 4  // pushbutton 2 (GPIO4 D2) - IR nightime lamp;
+#define BTN_PIN1 5  // pushbutton 1 (GPIO3 D1) - UV & daytime lamps;
+#define BTN_PIN2 4  // pushbutton 2 (GPIO1 D2) - IR nightime lamp;
 #define BTN1 1	// button ID
 #define BTN2 2	// button ID
 ToggleButtonManager _btnManager;
@@ -27,7 +26,7 @@ void setup() {
 
   _btnManager.init(2);
   _btnManager.registerButton(BTN1, BTN_PIN1, handleButton);
-//  _btnManager.registerButton(BTN2, BTN_PIN2, handleButton);
+  _btnManager.registerButton(BTN2, BTN_PIN2, handleButton);
 
   os_timer_setfn(&myTimer, (os_timer_func_t *)timerCallback, NULL);
   os_timer_arm(&myTimer, _timerDelay, true);
